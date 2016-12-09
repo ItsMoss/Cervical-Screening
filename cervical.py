a4187795c1f30d83dcd251feac028ef62db4f1db
mmj9@duke.edu <mmj9@duke.edu>
2016-12-09 16:54:46 -0500

    extraction for RGB and grayscale a go!

--- cervical.py
+++ cervical.py
@@ -1,5 +1,8 @@
 # This file is for containing all functionality used in create_svm and main
 import cv2 as cv2
+from numpy import ones, uint8
+
+COLORMAX = 256
 
 
 def parse_SVM_CLA():
@@ -40,3 +43,48 @@ def read_image(image_name, color=True):
         image = cv2.imread(image_name, 0)
 
     return image
+
+
+def extract_RGB(image):
+    """
+    This function extracts red, green, and blue content from a color image
+
+    :return dict channels: intensity histograms for each color (R, G, B)
+    """
+    # NOTE. Should add logging error if color image is not provided
+    rows = len(image)
+    cols = len(image[0])
+
+    red = 0 * ones(COLORMAX, uint8)
+    green = 0 * ones(COLORMAX, uint8)
+    blue = 0 * ones(COLORMAX, uint8)
+
+    for row in range(rows):
+        for col in range(cols):
+            red[image[row][col][2]] += 1
+            green[image[row][col][1]] += 1
+            blue[image[row][col][0]] += 1
+
+    channels = {"red": red,
+                "green": green,
+                "blue": blue}
+
+    return channels
+
+
+def create_grayscale_channel(image):
+    """
+    This function creates grayscale intensity histogram from grayscale image
+
+    :return ndarray channel: intensity histogram for grayscale
+    """
+    # NOTE. Should add logging error if color image is provided
+    rows = len(image)
+    cols = len(image[0])
+
+    channel = 0 * ones(COLORMAX, uint8)
+    for row in range(rows):
+        for col in range(cols):
+            channel[image[row][col]] += 1
+
+    return channel
--- test_cervical.py
+++ test_cervical.py
@@ -3,6 +3,9 @@ import cervical as cer
 import helpers as helps
 import cv2 as cv2
 from numpy import ones, uint8
+from random import randrange
+
+COLORMAX = 256
 
 
 def test_read_image():
@@ -36,3 +39,118 @@ def test_read_image():
             except TypeError:
                 assert gray_image[i][j] >= 0
                 assert gray_image[i][j] < 256
+
+
+def test_extract_RGB():
+    """
+    Tests functionality of extract_RGB from cervical.py
+    """
+    # Test image matrix 1 (random)
+    topleft = (randrange(COLORMAX), randrange(COLORMAX), randrange(COLORMAX))
+    topright = (randrange(COLORMAX), randrange(COLORMAX), randrange(COLORMAX))
+    botleft = (randrange(COLORMAX), randrange(COLORMAX), randrange(COLORMAX))
+    botright = (randrange(COLORMAX), randrange(COLORMAX), randrange(COLORMAX))
+
+    test1 = ones((2, 2, 3), uint8)
+    test1[0][0] = topleft
+    test1[0][1] = topright
+    test1[1][0] = botleft
+    test1[1][1] = botright
+
+    output1 = cer.extract_RGB(test1)
+    assert output1["red"][topleft[2]] > 0
+    assert output1["red"][topright[2]] > 0
+    assert output1["red"][botleft[2]] > 0
+    assert output1["red"][botright[2]] > 0
+    assert sum(output1["red"]) == 4
+    assert len(output1["red"]) == COLORMAX
+    assert output1["green"][topleft[1]] > 0
+    assert output1["green"][topright[1]] > 0
+    assert output1["green"][botleft[1]] > 0
+    assert output1["green"][botright[1]] > 0
+    assert sum(output1["green"]) == 4
+    assert len(output1["green"]) == COLORMAX
+    assert output1["blue"][topleft[0]] > 0
+    assert output1["blue"][topright[0]] > 0
+    assert output1["blue"][botleft[0]] > 0
+    assert output1["blue"][botright[0]] > 0
+    assert sum(output1["blue"]) == 4
+    assert len(output1["blue"]) == COLORMAX
+
+    # Test image matrix 2 (random)
+    topleft = (randrange(COLORMAX), randrange(COLORMAX), randrange(COLORMAX))
+    topright = (randrange(COLORMAX), randrange(COLORMAX), randrange(COLORMAX))
+    botleft = (randrange(COLORMAX), randrange(COLORMAX), randrange(COLORMAX))
+    botright = (randrange(COLORMAX), randrange(COLORMAX), randrange(COLORMAX))
+
+    test2 = ones((2, 2, 3), uint8)
+    test2[0][0] = topleft
+    test2[0][1] = topright
+    test2[1][0] = botleft
+    test2[1][1] = botright
+
+    output2 = cer.extract_RGB(test2)
+    assert output2["red"][topleft[2]] > 0
+    assert output2["red"][topright[2]] > 0
+    assert output2["red"][botleft[2]] > 0
+    assert output2["red"][botright[2]] > 0
+    assert sum(output2["red"]) == 4
+    assert len(output1["red"]) == COLORMAX
+    assert output2["green"][topleft[1]] > 0
+    assert output2["green"][topright[1]] > 0
+    assert output2["green"][botleft[1]] > 0
+    assert output2["green"][botright[1]] > 0
+    assert sum(output2["green"]) == 4
+    assert len(output1["green"]) == COLORMAX
+    assert output2["blue"][topleft[0]] > 0
+    assert output2["blue"][topright[0]] > 0
+    assert output2["blue"][botleft[0]] > 0
+    assert output2["blue"][botright[0]] > 0
+    assert sum(output2["blue"]) == 4
+    assert len(output1["blue"]) == COLORMAX
+    assert len(output1) == len(output2)
+
+
+def test_create_grayscale_channel():
+    """
+    Tests functionality of create_grayscale_channel from cervical.py
+    """
+    # Test image matrix 1 (random)
+    topleft = (randrange(COLORMAX))
+    topright = (randrange(COLORMAX))
+    botleft = (randrange(COLORMAX))
+    botright = (randrange(COLORMAX))
+
+    test1 = ones((2, 2), uint8)
+    test1[0][0] = topleft
+    test1[0][1] = topright
+    test1[1][0] = botleft
+    test1[1][1] = botright
+
+    output1 = cer.create_grayscale_channel(test1)
+    assert output1[topleft] > 0
+    assert output1[topright] > 0
+    assert output1[botleft] > 0
+    assert output1[botright] > 0
+    assert sum(output1) == 4
+    assert len(output1) == COLORMAX
+
+    # Test image matrix 2 (random)
+    topleft = (randrange(COLORMAX))
+    topright = (randrange(COLORMAX))
+    botleft = (randrange(COLORMAX))
+    botright = (randrange(COLORMAX))
+
+    test2 = ones((2, 2), uint8)
+    test2[0][0] = topleft
+    test2[0][1] = topright
+    test2[1][0] = botleft
+    test2[1][1] = botright
+
+    output2 = cer.create_grayscale_channel(test2)
+    assert output2[topleft] > 0
+    assert output2[topright] > 0
+    assert output2[botleft] > 0
+    assert output2[botright] > 0
+    assert sum(output2) == 4
+    assert len(output2) == COLORMAX

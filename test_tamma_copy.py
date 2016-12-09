0ad8c04fbf2cebfdda5f79a473d36c1ad7c11129
tk114@duke.edu <tk114@duke.edu>
2016-12-09 00:33:10 -0500

    create unit test for processing blue channel data

--- test_tamma_copy.py
+++ test_tamma_copy.py
@@ -36,3 +36,35 @@ def test_remove_glare_wronginput():
     expected_output2 = [1]*241 + [0]*15
     output2 = tm.remove_glare(input2, 240)
     assert output2 == expected_output2
+
+
+def test_stat_analysis():
+    """
+    Tests for stat_analysis functionality from tamma_copy.py
+    """
+
+    input1 = [1] * 241 + [0] * 15
+    expected_output1 = [1.0, 1.0, 1.0, 0.0]
+    output1 = tm.stat_analysis(input1)
+    assert output1 == expected_output1
+
+    input2 = list(range(241)) + [0] * 15
+    expected_output2 = [120.0, 0.0, 120.0, 69.7]
+    output1 = tm.stat_analysis(input2)
+    assert output2 == expected_output2
+
+    input3 = [0]*15 + list(range(226)) + [0] * 15
+    expected_output3 = [112.5, 0.0, 112.5, 65.4]
+    output3 = tm.stat_analysis(input3)
+    assert output3 == expected_output3
+
+    input4 = [0]*15 + list(range(101)) + [0] * 5 \
+             + list(range(150)) + [0] * 15
+    expected_output4 = [64.6, 0.0, 62, 40.2]
+    output4 = tm.stat_analysis(input4)
+    assert output4 == expected_output4
+
+    input5 = [0] * 100 + [1] * 50 + [2] * 150
+    expected_output5 = [1.6, 2.0, 2.0, 0.5]
+    output5 = tm.stat_analysis(input5)
+    assert output5 == expected_output5

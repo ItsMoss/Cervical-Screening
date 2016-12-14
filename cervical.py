2e5bf83f9cf32ff93d85af9b7b06883cb2d9e07d
tk114@duke.edu <tk114@duke.edu>
2016-12-14 13:43:16 -0500

    Remove specular reflection from blue channels data

--- cervical.py
+++ cervical.py
@@ -139,3 +139,24 @@ def channel_stats(channel):
              "mean": mean}
 
     return stats
+
+
+def remove_glare(input, threshold):
+    """
+    Remove reflective glare from blue channels data
+
+    :param input: List of RGB values from 0-255 (list)
+    :param threshold: threshold for removing glare (int)
+    :return: output: List of RGB values from 0-255 after removing glare
+    """
+
+    # Check the size of input list
+    if len(input) != 256:
+        addlist = [0]*(256-len(input))
+        input = input+addlist
+
+    # Remove reflective glare by removing values greater than threshold
+    output = input
+    output[threshold+1:] = [0] * (255-threshold)
+
+    return output
